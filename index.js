8d1def87ee5a53c9fdf7438d6ad3dc45b5e839f6
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-28 18:37:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function showRepositories(event, data) {
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
+
+function getCommits(el) {
+  const name = el.dataset.repoList
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
