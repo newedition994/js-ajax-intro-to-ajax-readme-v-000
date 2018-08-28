c38e8a6e11d425401c82eb70cbca8e39e5f1c600
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-28 18:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function getRepositories() {
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href')
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`)
 }
