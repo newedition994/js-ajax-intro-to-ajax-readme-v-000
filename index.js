82bc8b483e826436c51f9e51e4df3248eb309278
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-28 18:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function getCommits(el) {
 
 function showCommits() {
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${}</ul>`
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
 }
